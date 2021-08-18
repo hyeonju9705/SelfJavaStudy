0fb3766e6227b1aa7e622b03ba9c33b57d615191
정현주 <48791736+hyeonju9705@users.noreply.github.com>
2021-08-18 23:40:23 +0900

    ~576p / ~HashMap

---
+++ chap13/src/sec01/exam07/HashMapExample.java
@@ -0,0 +1,19 @@
+package sec01.exam07;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		// 학번과 이름이 동일한 경우 같은 키로 인식
+		Map<Student, Integer> map = new HashMap<Student, Integer>();
+		
+		map.put(new Student(1,"홍길동"),95);
+		map.put(new Student(1,"홍길동"), 95);
+		
+		System.out.println("총 Entry 수 : "+map.size());
+
+	}
+
+}
